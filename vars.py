331dbf49261a32a096e63766c222fae58b2c96e5
sameerji0911 <shammaarbaz@gmail.com>
2025-07-15 21:42:14 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,7 +13,7 @@ CREDIT = environ.get("CREDIT", "❤️𝗗𝗿. 𝗖𝗬𝗥𝗨𝗦 💚💖")
 TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '7714608838').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7431004177').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
